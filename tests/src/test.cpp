99c2cd80ea55e32d3fd8cc31ce6016d61079aef9
Takayama Fumihiko <tekezo@pqrs.org>
2025-09-27 10:36:23 +0900

    Add unwrap_not_null, make_weak, hash

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,5 +1,6 @@
 #include <boost/ut.hpp>
 #include <pqrs/gsl.hpp>
+#include <unordered_map>
 
 int main(void) {
   using namespace boost::ut;
@@ -9,6 +10,22 @@ int main(void) {
   "not_null_shared_ptr_t"_test = [] {
     pqrs::not_null_shared_ptr_t<std::string> p = std::make_shared<std::string>("hello");
     expect("hello"sv == *p);
+  };
+
+  "unwrap_not_null"_test = [] {
+    pqrs::not_null_shared_ptr_t<std::string> p = std::make_shared<std::string>("hello");
+    expect("hello"sv == *(pqrs::unwrap_not_null(p)));
+  };
+
+  "make_weak"_test = [] {
+    pqrs::not_null_shared_ptr_t<std::string> p = std::make_shared<std::string>("hello");
+    auto w = pqrs::make_weak(p);
+    expect("hello"sv == *(w.lock()));
+  };
+
+  "hash"_test = [] {
+    std::unordered_map<pqrs::not_null_shared_ptr_t<std::string>, std::string> map;
+    map.insert({std::make_shared<std::string>("hello"), "world"});
   };
 
   return 0;
